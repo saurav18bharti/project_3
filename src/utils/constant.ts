06c7cb0f58b2110e427128ef7509b75713b30525
saurav <saurav.delta4infotech@gmail.com>
2024-07-11 19:05:07 +0530

    minor fix in two-step-verification

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,13 +1,6 @@
-export const app_style =
-  "bg-card-bg-color flex flex-col justify-center items-center gap-8 rounded-2xl max-w-[789px] py-16 mx-4 my-12 md:mx-auto  ";
+export const app_style="bg-card-bg-color flex flex-col justify-center items-center gap-8 rounded-2xl max-w-[789px] py-16 mx-4 my-12 md:mx-auto  "
+export const max_width_content ="max-w-[28rem]"
+export const button_style = "text-sm xl:text-sm bg-header-button-2 py-3 w-full rounded-xl"
+export const input_style = "text-sm  py-2 px-3 md:py-3 md:px-4 border border-apple_google_border_color opacity-50 rounded-xl outline-none text-white placeholder-card_text_color  bg-input-bg-color "
 
-export const max_width_content = "max-w-[28rem]";
-
-export const button_style =
-  "text-sm xl:text-sm bg-header-button-2 py-3 w-full rounded-xl hover:bg-green-500 hover:text-white ";
-
-export const input_style =
-  "text-sm  py-2 px-3 md:py-3 md:px-4 border border-apple_google_border_color opacity-50 rounded-xl outline-none text-white placeholder-card_text_color  bg-input-bg-color ";
-
-export const otp_style =
-  "max-w-12 p-3 rounded-xl bg-black bg-opacity-5 border border-white border-opacity-20 text-white  ";
+export const otp_style = "max-w-12 p-3 rounded-xl bg-black bg-opacity-5 border border-white border-opacity-20 text-white"
